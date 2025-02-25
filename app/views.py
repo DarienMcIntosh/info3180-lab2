4beba21684ae34e6862c2b33c97d947996283a81
Darien McIntosh <darienmcintosh14@gmail.com>
2025-02-25 12:19:29 -0500

    Starter Flask App

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from app import app
-from flask import render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
+from datetime import datetime
 
-
+app = Flask(__name__)
 ###
 # Routing for your application.
 ###
@@ -16,6 +16,13 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    """Render the website's profile page."""
+    joined_date = date(2025, 2, 1)
+    formatted_joined_date = format_date_joined(joined_date)
+    return render_template('profile.html', joined_date=formatted_joined_date)
 
 
 ###
@@ -45,3 +52,14 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+def format_date_joined(date_obj): 
+    if not isinstance(date_obj, date):
+        return None 
+
+    formatted_date = date_obj.strftime("%B, %Y")
+    return formatted_date
+
+if __name__ == '__main__':
+    app.run(debug=True)
